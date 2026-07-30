b040c3ed183bd0114a1c1f786e0f620c417041ee
nongducthuan <150163180+nongducthuan@users.noreply.github.com>
2026-07-30 09:59:11 +0700

    Initial commit

--- backend/prisma/client.ts
+++ backend/prisma/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../src/generated/prisma';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import 'dotenv/config';
 
